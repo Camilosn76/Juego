0b24ab97731f34477ecadeb65e4b772398c0a101
Camilosn76 <73195441+Camilosn76@users.noreply.github.com>
2024-08-07 03:05:54 -0500

    Create Libreries.py

---
+++ Libreries.py
@@ -0,0 +1,8 @@
+!wget https://raw.githubusercontent.com/Camilosn76/Preguntas/main/Preguntas.py
+!wget https://raw.githubusercontent.com/Camilosn76/Juego/main/Retos.py
+!wget https://raw.githubusercontent.com/Camilosn76/Juego/main/Verdades.py
+from IPython.display import display, HTML
+import random
+from Preguntas import Preguntas
+from Retos import Retos
+from Verdades import Verdades
